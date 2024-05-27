da6ca7cc3560be0057ea4710d95a34cc1ab9c4e8
Thomas Aldaitz <taldaitz@dawan.fr>
2024-05-27 16:46:07 +0200

    add a HTML skeleton to the index page to fix the bug

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
-<h1>Projet git</h1>
-<h2>Présentation</h2>
-<p>
-    Texte de présentation plus détaillé
-</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-<footer>
-    <a href="mailto:taldaitz@dawan.fr">taldaitz@dawan.fr</a>
-</footer>
+    <h1>Projet git</h1>
+    <h2>Présentation</h2>
+    <p>
+        Texte de présentation plus détaillé
+    </p>
+
+    <footer>
+        <a href="mailto:taldaitz@dawan.fr">taldaitz@dawan.fr</a>
+    </footer>
+    
+</body>
+</html>
